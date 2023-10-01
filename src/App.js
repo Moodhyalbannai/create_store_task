ee5d5f432ded15d89b96e72bdb177725d0c083ba
Moodhyalbannai <mood_92@hotmail.com>
2023-10-01 20:49:27 +0300

    push task 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+import products from "./products"
 
 function App() {
 
+  const productsLists = products.map ((product)=>{
+    return(
+
+      <div className = "card">
+        <img src={product.image}></img>
+        <div class="titleAndPrice">
+        <h3>{product.name}</h3>
+        <p>{product.price}</p>
+          </div>
+      </div>
+
+    )
+  })
   
   return (
+
     <div>
 
 
 
-    <div className="App">
-<div className="App-header">
-    <h1>My Furniture Store</h1>
-    <h4>Fin!</h4>
-    <img src="https://cdn.shopify.com/s/files/1/0568/6362/1297/files/Parasmus-2.2_cdbf7619-1bf5-4d58-805a-33bfb9b6070b.jpg?v=1676368658"/>
+  <div className="App">
+    <div className="App-header">
+      <h1>My Furniture Store</h1>
+      <h4>Fin!</h4>
+
+      <div class="lazzoni-store-frame">
+      <img class="lazzoni-store-img" src="https://cdn.shopify.com/s/files/1/0568/6362/1297/files/Parasmus-2.2_cdbf7619-1bf5-4d58-805a-33bfb9b6070b.jpg?v=1676368658"/>
+      </div>
+
     </div>
-    </div>
+
+  </div>
+    <div class="card-frame">{productsLists}</div>
 
 
 
---
+++ src/products.js
@@ -0,0 +1,20 @@
+
+const products = [
+    {
+        name: "Haven Loft Leather",
+        price: "400 KD",
+        image: "https://www.westelm.com.kw/assets/styles/VariantProductImages/PW7139505/image-thumb__197312__product_zoom_large_800x800/202318_0076_img20z.jpg"
+    },
+    {
+        name: "Addie Sofa",
+        price: "500 KD",
+        image: "https://www.westelm.com.kw/assets/styles/VariantProductImages/130982647/image-thumb__203323__product_zoom_large_800x800/130982647_sku_hero_image_url.jpg"
+    },
+    {
+        name: "Navy Sofa",
+        price: "250 KD",
+        image: "https://www.westelm.com.kw/assets/styles/VariantProductImages/PW2875619/image-thumb__201814__product_zoom_large_800x800/202302_0188_img82z.jpg"
+    }
+]
+
+export default products;
