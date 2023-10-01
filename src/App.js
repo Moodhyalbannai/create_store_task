610ca2671b675925dfe454c9941f944ab6e4481f
Moodhyalbannai <mood_92@hotmail.com>
2023-10-01 19:35:35 +0300

    task 1 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  
   return (
+    <div>
+
+
+
     <div className="App">
-<div className="Container">
+<div className="App-header">
     <h1>My Furniture Store</h1>
-    <h4>This is the best furniture store!</h4>
+    <h4>Fin!</h4>
     <img src="https://cdn.shopify.com/s/files/1/0568/6362/1297/files/Parasmus-2.2_cdbf7619-1bf5-4d58-805a-33bfb9b6070b.jpg?v=1676368658"/>
     </div>
+    </div>
+
+
+
     </div>
   );
 }
