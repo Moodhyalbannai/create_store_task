ab87182d2eda100eb27420ae178a7725e5cb6001
Moodhyalbannai <mood_92@hotmail.com>
2023-10-02 18:21:44 +0300

    home component done

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import products from "./products"
+import logo from "./logo.svg";
+import "./App.css";
+import products from "./products";
+import { Home } from "./components/Home";
 
 function App() {
-
-  const productsLists = products.map ((product)=>{
-    return(
-
-      <div className = "card">
+  const productsLists = products.map((product) => {
+    return (
+      <div className="card">
         <img src={product.image}></img>
         <div className="titlePrice">
-        <h3>{product.name}</h3>
-        <p>{product.price}</p>
-          </div>
+          <h3>{product.name}</h3>
+          <p>{product.price}</p>
+        </div>
       </div>
+    );
+  });
 
-    )
-  })
-  
   return (
-
     <div>
-
-
-
-  <div className="App">
-    
-    <div className="App-header">
-      
-        <div className="logo-storeName-slogan-frame">
-          <div className="logo-storeName">
-            <img className="logo" src="https://www.nicepng.com/png/detail/17-178633_nest-logo-chair-png-from-coefficient-design-interiors.png"></img>
-            <h1>Lazzoni</h1>
-          </div>
-          <h4>Crafted with Excellence</h4>
-          </div>
-
-          <div className="shopping">
-            <img src="furniture_store/src/logo.svg"/>
-          </div>
-      
-      <div className="shadow-overlay"></div>
-     
-
-    </div>
-
-  </div>
-    <div className="card-frame">{productsLists}</div>
-
-
-
+      <Home />
+      <div className="card-frame">{productsLists}</div>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+const Home = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="logo-storeName-slogan-frame">
+          <div className="logo-storeName">
+            <img
+              className="logo"
+              src="https://www.nicepng.com/png/detail/17-178633_nest-logo-chair-png-from-coefficient-design-interiors.png"
+            ></img>
+            <h1>Lazzoni</h1>
+          </div>
+          <h4>Crafted with Excellence</h4>
+        </div>
+
+        <div className="shopping">
+          <img src="furniture_store/src/logo.svg" />
+        </div>
+
+        <div className="shadow-overlay"></div>
+      </div>
+    </div>
+  );
+};
+
+export { Home };
